c5194f5074325668db03cf13441a9fedf26fb399
harada <test@test>
2019-04-28 16:56:28 +0900

    tool add

---
+++ CollisionTool/src/Vector.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct Vector2
+{
+	Vector2() : x(0), y(0) {}
+	Vector2(int IN_x, int IN_y) : x(IN_x), y(IN_y) {}
+	int x, y;
+};
+
---
+++ CollisionTool/src/main.cpp
@@ -0,0 +1,90 @@
+
+#include <DxLib.h>
+#include <string>
+#include "InputManager.h"
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+#define IMAGE_OFFSET_X (WINDOW_WIDTH / 2 - 50)
+#define IMAGE_OFFSET_Y (WINDOW_HEIGHT / 2 - 50)
+
+int main(int argc, char* argv[])
+{
+	SetGraphMode(WINDOW_WIDTH, WINDOW_HEIGHT, 32);
+	ChangeWindowMode(true);
+	if (DxLib_Init() == -1)return false;
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	SetDragFileValidFlag(TRUE);
+
+	// 一時的な変数(あとでクラスにする予定)
+	char filePath[MAX_PATH];
+	std::string imagePath;
+	bool inputFlag = false;
+	int texHandle = -1;
+	InputManager inputData;
+	Vector2 boxStartPos(-1,-1);
+
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+		ClsDrawScreen();
+		
+		inputData.Update();
+
+		if (inputFlag)
+		{
+			if (inputData.MouseStateDown(MOUSE_INPUT_LEFT))
+			{
+				boxStartPos = inputData.MousePos();
+			}
+
+			if (inputData.MouseStateUp(MOUSE_INPUT_LEFT))
+			{
+				boxStartPos = Vector2(-1, -1);
+			}
+
+			if (texHandle != -1)
+			{
+				DrawGraph(IMAGE_OFFSET_X, IMAGE_OFFSET_Y, texHandle, true);
+			}
+
+			if (!(boxStartPos.x == -1 && boxStartPos.y == -1))
+			{
+				auto mousePos = inputData.MousePos();
+				DrawBox(boxStartPos.x, boxStartPos.y, mousePos.x, mousePos.y, 0xffffff, false);
+			}
+		}
+		else
+		{
+			if (GetDragFileNum() > 0)
+			{
+				GetDragFilePath(filePath);
+
+				texHandle = LoadGraph(filePath);
+
+				imagePath = filePath;
+
+				auto findNum = imagePath.rfind("image");
+
+				imagePath = imagePath.substr(findNum);
+
+				auto i = imagePath.find("\\");
+				std::string replace = "/";
+				imagePath.replace(i, 1, replace);
+
+				inputFlag = true;
+
+				DrawString(0, 0, imagePath.c_str(), 0xffffff);
+			}
+
+			DrawString(200, IMAGE_OFFSET_Y, "画像ファイルをドラッグ&ドロップしてください", 0xffffff);
+		}
+
+		ScreenFlip();
+	}
+
+	DxLib_End();
+
+	return 0;
+}
