d8fb75e150920202ba84d4d3d4be312018ac71b3
サクサクパンダ <sakumeikou@icloud.com>
2019-04-28 13:12:55 +0900

    teamprivate削除

--- teamPrivate/source/DIR.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-enum class DIR {
-	RIGHT,
-	LEFT,
-	MAX
-};
--- teamPrivate/source/LOCATE.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-enum class LOCATE {
-	NON,		// ‹ó’†
-	GROUND,		// ’n–Ê
-	FLOOR,		// •‚‚¢‚Ä‚¢‚é°
-	MAX
-};
