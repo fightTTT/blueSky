b7f8346d7c1ae5fd20787cb1013fde6bd8c88118
harada <test@test>
2019-04-29 14:24:08 +0900

    ファイル整理

---
+++ ProjectBlueSky/src/Character.cpp
@@ -0,0 +1,54 @@
+#include "Dxlib.h"
+#include "ImageMng.h"
+#include "Character.h"
+
+
+Character::Character()
+{
+}
+
+Character::~Character()
+{
+}
+
+bool Character::InitAnim(void)
+{
+	AddAnim("‘Ò‹@", 0, 0, 10, 5, true);
+	SetAnim("‘Ò‹@");
+	return true;
+}
+
+bool Character::CheckObjType(OBJ_TYPE type)
+{
+	return (type == OBJ_TYPE_CHARACTER);
+}
+
+void Character::Draw(void)
+{
+	if (imageName.length() == 0)
+	{
+		return;
+	}
+	int id = 0;
+	if (animTable.find(animName) != animTable.end())
+	{
+		id = animCnt / animTable[animName][ANIM_TBL_INV];
+		if (animTable[animName][ANIM_TBL_LOOP] || id < animTable[animName][ANIM_TBL_FRAME])
+		{
+			id %= animTable[animName][ANIM_TBL_FRAME];
+		}
+		else
+		{
+			id = animTable[animName][ANIM_TBL_FRAME] - 1;
+			animEndFlag = true;
+		}
+	}
+
+	imageName = ("image/" + characterName + "/" + GetAnim() + "/stand_" + std::to_string(id) + ".png");
+
+	if (visible)
+	{
+		DrawGraph(drawOffset.x + pos.x, drawOffset.y + pos.y, IMAGE_ID(imageName)[0], true);
+	}
+	animCnt++;
+}
---
+++ ProjectBlueSky/src/Ryu.cpp
@@ -0,0 +1,13 @@
+#include "Ryu.h"
+
+Ryu::Ryu()
+{
+	characterName = "ƒŠƒ…ƒE";
+
+	Init("image/ƒŠƒ…ƒE/‘Ò‹@/stand_0.png", VECTOR2(290, 178), VECTOR2(1, 1), VECTOR2(300, 300));
+	InitAnim();
+}
+
+Ryu::~Ryu()
+{
+}
