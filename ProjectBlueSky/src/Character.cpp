d791e632286d387124aa446fa4e2af9a592799dc
tonki <tonkiHouse>
2019-05-07 16:41:58 +0900

    アニメーション情報の追加

--- ProjectBlueSky/src/Character.cpp
+++ ProjectBlueSky/src/Character.cpp
@@ -22,13 +22,23 @@ bool Character::Init(std::string fileName, VECTOR2 divSize, VECTOR2 divCut, VECT
 	animFileName["ジャンプ_上"] = "jump";
 	animFileName["ジャンプ_前"] = "jump_front";
 	animFileName["ジャンプ_後ろ"] = "jump_back";
+	animFileName["パンチ_小"] = "punch_small";
+	animFileName["パンチ_大"] = "punch_big";
+	animFileName["波動拳"] = "hadoken";
+	animFileName["竜巻旋風脚"] = "tatsumaki_senpukyaku";
+	animFileName["昇竜拳"] = "syoryuken";
 
 	std::vector<std::string> animName = { "待機",
 										  "前移動",
 										  "後ろ移動",
 										  "ジャンプ_上",
 										  "ジャンプ_前",
-										  "ジャンプ_後ろ" };		// ｱﾆﾒｰｼｮﾝ名を要素として持つvector
+										  "ジャンプ_後ろ",
+										  "パンチ_小", 
+										  "パンチ_大", 
+										  "波動拳", 
+										  "竜巻旋風脚", 
+										  "昇竜拳"};		// ｱﾆﾒｰｼｮﾝ名を要素として持つvector
 
 	lpImageMng.LoadImageCharacterAll(characterName, animName, animFileName);
 
@@ -43,6 +53,11 @@ bool Character::InitAnim(void)
 	AddAnim("ジャンプ_上", 0, 0, 12, 5, true);
 	AddAnim("ジャンプ_前", 0, 0, 14, 5, true);
 	AddAnim("ジャンプ_後ろ", 0, 0, 14, 5, true);
+	AddAnim("パンチ_小", 0, 0, 4, 5, false);
+	AddAnim("パンチ_大", 0, 0, 7, 5, false);
+	AddAnim("波動拳", 0, 0, 12, 5, false);
+	AddAnim("竜巻旋風脚", 0, 0, 13, 5, false);
+	AddAnim("昇竜拳", 0, 0, 14, 5, false);
 	SetAnim("待機");
 	return true;
 }
