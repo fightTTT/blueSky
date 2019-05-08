227c2ea31ef1dcabc9efb0d987feabbbee83f5c7
tonki <tonkiHouse>
2019-05-08 12:47:34 +0900

    アニメーション情報の追加、技のアニメーション情報の取り扱い方の修正

--- ProjectBlueSky/src/Character.cpp
+++ ProjectBlueSky/src/Character.cpp
@@ -16,17 +16,20 @@ bool Character::Init(std::string fileName, VECTOR2 divSize, VECTOR2 divCut, VECT
 	Obj::Init(fileName, divSize, divCut, pos, turn);
 	InitAnim();
 
+	// 通常のアクション
 	animFileName["待機"] = "stand";
 	animFileName["前移動"] = "move_front";
 	animFileName["後ろ移動"] = "move_back";
 	animFileName["ジャンプ_上"] = "jump";
 	animFileName["ジャンプ_前"] = "jump_front";
 	animFileName["ジャンプ_後ろ"] = "jump_back";
+	animFileName["しゃがみ"] = "squat";
+	animFileName["しゃがみ_後ろ"] = "squat_back";
+	animFileName["しゃがみ始め"] = "squat_start";
+	animFileName["立ち始め"] = "stand_start";
 	animFileName["パンチ_小"] = "punch_small";
 	animFileName["パンチ_大"] = "punch_big";
-	animFileName["波動拳"] = "hadoken";
-	animFileName["竜巻旋風脚"] = "tatsumaki_senpukyaku";
-	animFileName["昇竜拳"] = "syoryuken";
+	animFileName["ガード_しゃがみ"] = "guard_squat";
 
 	std::vector<std::string> animName = { "待機",
 										  "前移動",
@@ -34,11 +37,30 @@ bool Character::Init(std::string fileName, VECTOR2 divSize, VECTOR2 divCut, VECT
 										  "ジャンプ_上",
 										  "ジャンプ_前",
 										  "ジャンプ_後ろ",
+										  "しゃがみ",
+										  "しゃがみ_後ろ",
+										  "しゃがみ始め",
+										  "立ち始め",
 										  "パンチ_小", 
-										  "パンチ_大", 
-										  "波動拳", 
-										  "竜巻旋風脚", 
-										  "昇竜拳"};		// ｱﾆﾒｰｼｮﾝ名を要素として持つvector
+										  "パンチ_大",
+										  "ガード_しゃがみ" };		// ｱﾆﾒｰｼｮﾝ名を要素として持つvector
+
+	// 必殺技系
+	if ((spAttackAnimName[0] != "技1") && (spAttackAnimFileName[0] != "waza_1"))
+	{
+		animFileName[spAttackAnimName[0]] = spAttackAnimFileName[0];
+		animName.push_back(spAttackAnimName[0]);
+	}
+	if ((spAttackAnimName[1] != "技2") && (spAttackAnimFileName[1] != "waza_2"))
+	{
+		animFileName[spAttackAnimName[1]] = spAttackAnimFileName[1];
+		animName.push_back(spAttackAnimName[1]);
+	}
+	if ((spAttackAnimName[2] != "技3") && (spAttackAnimFileName[2] != "waza_3"))
+	{
+		animFileName[spAttackAnimName[2]] = spAttackAnimFileName[2];
+		animName.push_back(spAttackAnimName[2]);
+	}
 
 	lpImageMng.LoadImageCharacterAll(characterName, animName, animFileName);
 
@@ -53,11 +75,13 @@ bool Character::InitAnim(void)
 	AddAnim("ジャンプ_上", 0, 0, 12, 5, true);
 	AddAnim("ジャンプ_前", 0, 0, 14, 5, true);
 	AddAnim("ジャンプ_後ろ", 0, 0, 14, 5, true);
+	AddAnim("しゃがみ", 0, 0, 4, 5, true);
+	AddAnim("しゃがみ_後ろ", 0, 0, 1, 5, true);
+	AddAnim("しゃがみ始め", 0, 0, 2, 5, false);
+	AddAnim("立ち始め", 0, 0, 2, 5, false);
 	AddAnim("パンチ_小", 0, 0, 4, 5, false);
 	AddAnim("パンチ_大", 0, 0, 7, 5, false);
-	AddAnim("波動拳", 0, 0, 11, 5, false);
-	AddAnim("竜巻旋風脚", 0, 0, 13, 5, false);
-	AddAnim("昇竜拳", 0, 0, 14, 5, false);
+	AddAnim("ガード_しゃがみ", 0, 0, 1, 5, true);
 	SetAnim("待機");
 	return true;
 }
