0092466583628f6bed429a39d11142783506cc31
harada <test@test>
2019-05-09 11:30:38 +0900

    AIclass追加

---
+++ ProjectBlueSky/src/AIRyu.cpp
@@ -0,0 +1,26 @@
+#include "AIRyu.h"
+
+AIRyu::AIRyu(VECTOR2 pos, VECTOR2 offset, DIR charaDir)
+{
+	characterName = "ƒŠƒ…ƒE";
+
+	AddAnim("”g“®Œ", 0, 0, 11, 5, false);
+	spAttackAnimName[0] = "”g“®Œ";
+	spAttackAnimFileName[0] = "hadoken";
+
+	AddAnim("—³Šªù•—‹r", 0, 0, 13, 5, false);
+	spAttackAnimName[1] = "—³Šªù•—‹r";
+	spAttackAnimFileName[1] = "tatsumaki_senpukyaku";
+
+	AddAnim("¸—³Œ", 0, 0, 14, 5, false);
+	spAttackAnimName[2] = "¸—³Œ";
+	spAttackAnimFileName[2] = "syoryuken";
+
+	Init("image/ƒŠƒ…ƒE/‘Ò‹@/stand_0.png", VECTOR2(290, 178), VECTOR2(1, 1), VECTOR2(pos.x, pos.y), false);
+
+	dir = charaDir;
+}
+
+AIRyu::~AIRyu()
+{
+}
--- ProjectBlueSky/src/GameScene.cpp
+++ ProjectBlueSky/src/GameScene.cpp
@@ -5,6 +5,7 @@
 //#include "SceneMng.h"
 #include "GameScene.h"
 #include "GameCtrl.h"
+#include "AIRyu.h"
 
 
 GameScene::GameScene()
@@ -27,16 +28,23 @@ unique_Base GameScene::UpDate(unique_Base own, const GameCtrl & controller)
 	auto deth_itr = std::remove_if(objList->begin(), objList->end(), [](std::shared_ptr<Obj> obj) {return obj->CheckDeth(); });
 	objList->erase(deth_itr, objList->end());
 
-	sharedObj sObj[2];
-	int charaCount = 0;
+	sharedObj sObj[2];	// キャラクターのObj変数保存
+	int charaCount = 0;	// 見つかったキャラクターの数
+	OBJ_TYPE type[2];	// キャラクターのタイプ
 
 	for (auto& data : *objList)
 	{
 		if (data->CheckObjType(OBJ_TYPE_CHARACTER))
 		{
 			sObj[charaCount] = data;
+			type[charaCount] = OBJ_TYPE_CHARACTER;
 			charaCount++;
-
+		}
+		else if (data->CheckObjType(OBJ_TYPE_AICHARACTER))
+		{
+			sObj[charaCount] = data;
+			type[charaCount] = OBJ_TYPE_AICHARACTER;
+			charaCount++;
 		}
 
 		if (charaCount == 2)
@@ -50,6 +58,20 @@ unique_Base GameScene::UpDate(unique_Base own, const GameCtrl & controller)
 			{
 				sObj[0]->SetDir(DIR_RIGHT);
 				sObj[1]->SetDir(DIR_LEFT);
+			}
+
+			if (!(type[0] == type[1]))
+			{
+				if (type[0] == OBJ_TYPE_CHARACTER)
+				{
+					EnemyState state(sObj[0]->GetPos(), sObj[0]->GetAnim());
+					sObj[1]->SetEnemyState(state);
+				}
+				else
+				{
+					EnemyState state(sObj[1]->GetPos(), sObj[1]->GetAnim());
+					sObj[0]->SetEnemyState(state);
+				}
 			}
 		}
 	}
@@ -68,8 +90,9 @@ int GameScene::Init(void)
 	}
 	objList->clear();		// objListを全削除する
 
-	AddObjList()(objList, std::make_unique<Ryu>(VECTOR2(1440 - (290 / 2), 810), VECTOR2(-(290 / 2), -178), PAD_1, DIR_RIGHT));
-	AddObjList()(objList, std::make_unique<Ryu>(VECTOR2((290 / 2), 810), VECTOR2(-(290 / 2), -178), PAD_2, DIR_LEFT));
+	//AddObjList()(objList, std::make_unique<Ryu>(VECTOR2(1440 - (290 / 2), 810), VECTOR2(-(290 / 2), -178), PAD_2, DIR_RIGHT));
+	AddObjList()(objList, std::make_unique<Ryu>(VECTOR2((290 / 2), 810), VECTOR2(-(290 / 2), -178), PAD_1, DIR_RIGHT));
+	AddObjList()(objList, std::make_unique<AIRyu>(VECTOR2(1440 - (290 / 2), 810), VECTOR2(-(290 / 2), -178), DIR_LEFT));
 
 	//AddObjList()(objList, std::make_unique<Ryu>(lpSceneMng.GetDrawOffset(), popNo));
 	return 0;
