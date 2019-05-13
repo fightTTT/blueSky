f6588f6dbeb0c5f509a7b3191ecd396d910fc322
harada <test@test>
2019-05-13 15:34:54 +0900

    ガード一部追加

--- ProjectBlueSky/src/MoveState.cpp
+++ ProjectBlueSky/src/MoveState.cpp
@@ -8,6 +8,7 @@
 #include "DxLib.h"
 
 #define MOVE_SPEED (2)
+#define ATTACK_RANGE (150)
 
 MoveState::MoveState()
 {
@@ -21,28 +22,13 @@ MoveState::~MoveState()
 
 void MoveState::Init(AICharacter * character)
 {
-	if (initFlag)
+	if (!initFlag)
 	{
-		auto pos = character->GetPos();
-		auto enemy = character->GetEnemyState();
-
-		auto distance = enemy.enemyPos - pos;
-
-		if (abs(distance.x) < 100)
-		{
-			// 敵と距離が近い場合はなれる
-			moveDirFlag = false;
-		}
-		else
-		{
-			moveDirFlag = true;
-		}
-
-		stateTime = 0;
-
+		moveDirFlag = true;
 		initFlag = true;
 	}
-	moveDirFlag = true;
+
+	stateTime = 0;
 }
 
 void MoveState::Update(AICharacter * character)
@@ -55,6 +41,98 @@ void MoveState::Update(AICharacter * character)
 	VECTOR2 vec = enemy.enemyPos - pos;
 	int rand;
 
+	if (abs(vec.x) < ATTACK_RANGE && (enemy.enemyAnimName == "キック_大" || enemy.enemyAnimName == "キック_小"))
+	{
+		character->SetAnim("ガード_立ち");
+		return;
+	}
+
+	if (abs(vec.x) < ATTACK_RANGE && (enemy.enemyAnimName == "キック_大_しゃがみ" || enemy.enemyAnimName == "キック_小_しゃがみ"))
+	{
+		character->SetAnim("ガード_しゃがみ");
+		return;
+	}
+
+	// 近距離攻撃が当たる距離の場合攻撃
+	if (abs(vec.x) < 100)
+	{
+		rand = GetRand(5);
+
+		if (rand == 0)
+		{
+			character->ChangeState(AttackState::GetInstance());
+			character->SetAnim("パンチ_大");
+			return;
+		}
+	}
+	// 距離が遠い場合
+	else
+	{
+		rand = GetRand(1000);
+
+		if (rand == 0)
+		{
+			character->ChangeState(LongAttackState::GetInstance());
+			return;
+		}
+	}
+
+	// 弾をジャンプ回避
+	for (auto data : enemy.shotData)
+	{
+		// 自分が撃った弾の場合
+		if (data.id == character->GetPadID())
+		{
+			continue;
+		}
+
+		auto distance = pos - data.pos;
+
+		// 弾が自分より前にある場合実行
+		if ((charaDir == DIR_RIGHT && distance.x > -divSize.x) || (charaDir == DIR_LEFT && distance.x < divSize.x))
+		{
+			if (data.pos.x > 0 && abs(distance.x) < 200)
+			{
+				character->SetJumpType(JUMP_TYPE_RAND);
+				character->ChangeState(JumpState::GetInstance());
+				return;
+			}
+		}
+		else
+		{
+			continue;
+		}
+	}
+
+	rand = GetRand(500);
+
+	// 移動方向にジャンプ
+	if (moveDirFlag)
+	{
+		if (rand == 0)
+		{
+			character->SetJumpType(JUMP_TYPE_FRONT);
+			character->ChangeState(JumpState::GetInstance());
+			return;
+		}
+	}
+	else
+	{
+		if (rand == 0)
+		{
+			character->SetJumpType(JUMP_TYPE_BACK);
+			character->ChangeState(JumpState::GetInstance());
+			return;
+		}
+	}
+
+	//if (abs(vec.x) < 100)
+	//{
+	//	moveDirFlag = !(abs(vec.x) < 100);
+	//	stateTime = 0;
+	//}
+
+	// 移動処理
 	if (moveDirFlag)
 	{
 		// 前に進む
@@ -127,68 +205,4 @@ void MoveState::Update(AICharacter * character)
 
 	character->SetPos(pos);
 	stateTime++;
-
-	// 距離を再計算
-	vec = enemy.enemyPos - pos;
-
-	// 近距離攻撃が当たる距離の場合攻撃
-	if (abs(vec.x) < 100)
-	{
-		rand = GetRand(5);
-
-		if (rand == 0)
-		{
-			character->ChangeState(AttackState::GetInstance());
-			character->SetAnim("パンチ_大");
-			return;
-		}
-	}
-	// 距離が遠い場合
-	else/* if(abs(vec.x) > 100)*/
-	{
-		rand = GetRand(500);
-
-		if (rand == 0)
-		{
-			character->ChangeState(LongAttackState::GetInstance());
-			return;
-		}
-		
-	}
-
-	// 弾をジャンプ回避
-	for (auto data : enemy.shotData)
-	{
-		// 自分が撃った弾の場合
-		if (data.id == character->GetPadID())
-		{
-			continue;
-		}
-
-		auto distance = pos - data.pos;
-
-		// 弾が自分より前にある場合実行
-		if ((charaDir == DIR_RIGHT && distance.x > -divSize.x) || (charaDir == DIR_LEFT && distance.x < divSize.x))
-		{
-			if (data.pos.x > 0 && abs(distance.x) < 200)
-			{
-				character->SetJumpType(JUMP_TYPE_RAND);
-				character->ChangeState(JumpState::GetInstance());
-				return;
-			}
-		}
-		else
-		{
-			continue;
-		}
-	}
-
-	rand = GetRand(99);
-
-	if (abs(vec.x) < 100)
-	{
-		moveDirFlag = !(abs(vec.x) < 100);
-		stateTime = 0;
-	}
-
 }
