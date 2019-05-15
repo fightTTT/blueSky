88f796c989c6401d40bbd66d42fd0f208b804cf7
harada <test@test>
2019-05-15 15:40:18 +0900

    ノックバック追加

--- ProjectBlueSky/src/AIState.h
+++ ProjectBlueSky/src/AIState.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define COMBO_BREAK_CNT (3)
+
 class AICharacter;
 
 class AIState
--- ProjectBlueSky/src/DamageState.cpp
+++ ProjectBlueSky/src/DamageState.cpp
@@ -2,6 +2,8 @@
 #include "AICharacter.h"
 #include "MoveState.h"
 
+#define KNOCK_BACK_SPEED (10)
+
 DamageState::DamageState()
 {
 }
@@ -12,12 +14,48 @@ DamageState::~DamageState()
 
 void DamageState::Init(AICharacter * character)
 {
+	auto dir = character->GetDir();
+
+	if (dir == DIR_RIGHT)
+	{
+		knockBackSpeed = -KNOCK_BACK_SPEED;
+	}
+	else
+	{
+		knockBackSpeed = KNOCK_BACK_SPEED;
+	}
 }
 
 void DamageState::Update(AICharacter * character)
 {
+	auto dir = character->GetDir();
+	auto pos = character->GetPos();
+
 	if (character->GetAnimEndFlag())
 	{
 		character->ChangeState(MoveState::GetInstance());
 	}
+	else
+	{
+		pos.x += knockBackSpeed;
+
+		if (dir == DIR_RIGHT)
+		{
+			knockBackSpeed++;
+			if (knockBackSpeed > 0)
+			{
+				knockBackSpeed = 0;
+			}
+		}
+		else
+		{
+			knockBackSpeed--;
+			if (knockBackSpeed < 0)
+			{
+				knockBackSpeed = 0;
+			}
+		}
+	}
+
+	character->SetPos(pos);
 }
