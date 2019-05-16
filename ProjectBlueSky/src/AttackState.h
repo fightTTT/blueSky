1c20c9ef78e379560df277d455f1eb6e0331801b
harada <test@test>
2019-05-16 10:38:43 +0900

    攻撃中の当たり判定追加

--- ProjectBlueSky/src/AttackState.h
+++ ProjectBlueSky/src/AttackState.h
@@ -19,6 +19,6 @@ private:
 
 	void Init(AICharacter* character);
 	void Update(AICharacter* character);
-	void CheckHitFlag(AICharacter* character) {}
+	void CheckHitFlag(AICharacter* character);
 };
 
