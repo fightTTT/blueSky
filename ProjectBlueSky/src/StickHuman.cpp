fc0fc12bf8e69cdece5bd7d7ec2e67311aaed6c1
tonki <tonkiHouse>
2019-05-20 16:28:11 +0900

    棒人間(白)の技2を旋風脚に変更

--- ProjectBlueSky/src/StickHuman.cpp
+++ ProjectBlueSky/src/StickHuman.cpp
@@ -20,15 +20,15 @@ StickHuman::StickHuman(VECTOR2 pos, VECTOR2 offset, PAD_ID id, DIR charaDir) : C
 		spAttackCommand[0][DIR_LEFT].resize(3);
 		spAttackCommand[0][DIR_LEFT] = { COM_DIR_DOWN, COM_DIR_LEFT_DOWN, COM_DIR_LEFT };
 
-		AddAnim("ìäÇ∞", 0, 0, 13, 5, false, 0, 0);
-		spAttackAnimName[1] = "ìäÇ∞";
-		spAttackAnimFileName[1] = "throw";
-		spAttackType[1] = SKILL_TYPE_PUNCH;
+		AddAnim("ê˘ïóãr", 0, 0, 4, 5, true, 0, 0);
+		spAttackAnimName[1] = "ê˘ïóãr";
+		spAttackAnimFileName[1] = "senpu";
+		spAttackType[1] = SKILL_TYPE_KICK;
 
-		spAttackCommand[1][DIR_RIGHT].resize(2);
-		spAttackCommand[1][DIR_RIGHT] = { COM_DIR_DOWN, COM_DIR_ACCUMULATE };
-		spAttackCommand[1][DIR_LEFT].resize(2);
-		spAttackCommand[1][DIR_LEFT] = { COM_DIR_DOWN, COM_DIR_ACCUMULATE };
+		spAttackCommand[1][DIR_RIGHT].resize(3);
+		spAttackCommand[1][DIR_RIGHT] = { COM_DIR_DOWN, COM_DIR_LEFT_DOWN, COM_DIR_LEFT };
+		spAttackCommand[1][DIR_LEFT].resize(3);
+		spAttackCommand[1][DIR_LEFT] = { COM_DIR_DOWN, COM_DIR_RIGHT_DOWN, COM_DIR_RIGHT };
 
 		AddAnim("è∏ó≥", 0, 0, 16, 5, false, 0, 0);
 		spAttackAnimName[2] = "è∏ó≥";
