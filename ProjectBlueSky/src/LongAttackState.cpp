ef8f41fec05bbebc67ef6f0294c34fa65c2fd2bc
harada <test@test>
2019-05-24 13:56:55 +0900

    黒のバグ修正

--- ProjectBlueSky/src/LongAttackState.cpp
+++ ProjectBlueSky/src/LongAttackState.cpp
@@ -32,8 +32,29 @@ void LongAttackState::Update(AICharacter * character)
 		character->ChangeState(MoveState::GetInstance());
 		character->SetAnimStopFlag(false);
 	}
+	if (charaAnim == "ローリングアタック")
+	{
+		if (character->GetAnimCount() > 20)
+		{
+			if (dir == DIR_RIGHT)
+			{
+				pos.x += 20;
+			}
+			else
+			{
+				pos.x -= 20;
+			}
+		}
 
-	if (charaAnim == "ワープ")
+		if (character->GetAnimCount() > 60)
+		{
+			character->SetDirChange(true);
+			character->SetAnim("待機");
+			character->ChangeState(MoveState::GetInstance());
+			return;
+		}
+	}
+	else if (charaAnim == "ワープ")
 	{
 		if (character->GetAnimCount() == 31)
 		{
