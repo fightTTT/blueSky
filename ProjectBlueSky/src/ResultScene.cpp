89cce25ce94b93ba2e0fef17ac550a639b60a2d9
sakumeikou <mail@mail.com>
2019-05-28 18:15:40 +0900

    make

--- ProjectBlueSky/src/ResultScene.cpp
+++ ProjectBlueSky/src/ResultScene.cpp
@@ -1,8 +1,10 @@
+#include "DxLib.h"
 #include "ResultScene.h"
 #include "GameCtrl.h"
 #include "TitleScene.h"
-
-#include "DxLib.h"
+#include "ImageMng.h"
+#include "SceneMng.h"
+#include <string>
 
 ResultScene::ResultScene(bool leftFlag)
 {
@@ -22,25 +24,54 @@ unique_Base ResultScene::UpDate(unique_Base own, const GameCtrl & controller)
 		return std::make_unique<TitleScene>();
 	}
 
-	ResultDraw();
 
+
+	ResultDraw();
+	animCnt++;
 	return std::move(own);
 }
 
 int ResultScene::Init()
 {
+
+	charName = { "棒人間",
+				"棒人間_黒",
+				"棒人間_赤",
+				"棒人間_青",
+				"棒人間_緑",
+				"棒人間_黄",
+				"棒人間_ピンク",
+				"棒人間_水色" };
+
+	animCnt = 0;
+
 	return 0;
 }
 
 void ResultScene::ResultDraw()
 {
+	
+	DrawString(500, 350, "STARTボタンでタイトルへ遷移", 0xffffff);
+
+	std::string imagePass[2];
+
 	if (victoryFlag)
 	{
-		DrawString(500, 300, "1Pの勝ち！", 0xff0000);
+		imagePass[0] = ("image/" + charName[lpSceneMng.GetCharID(PAD_1)] + "/勝ち/"+"win_" + std::to_string(animCnt / 4 % 5) + ".png");
+		imagePass[1] = ("image/" + charName[lpSceneMng.GetCharID(PAD_2)] + "/負け/"+"lose_" + std::to_string(animCnt / 8 % 2) + ".png");
 	}
 	else
 	{
-		DrawString(500, 300, "2Pの勝ち！", 0xff0000);
+		imagePass[0] = ("image/" + charName[lpSceneMng.GetCharID(PAD_1)] + "/負け/"+"lose_" + std::to_string(animCnt / 8 % 2) + ".png");
+		imagePass[1] = ("image/" + charName[lpSceneMng.GetCharID(PAD_2)] + "/勝ち/"+"win_" + std::to_string(animCnt / 4 % 5) + ".png");
 	}
-	DrawString(500, 350, "STARTボタンでタイトルへ遷移", 0xffffff);
+
+
+
+	for (int num = 0; num < 2; num++)
+	{
+		int x = 1280 / 4 * (1 + (num * 2));
+		DrawRotaGraph(x, 720 / 2, 1.0, 0.0, lpImageMng.GetID(imagePass[num])[0], true, num);
+	}
+
 }
