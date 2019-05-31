7e57d69cf0c82c5af003de4e6a61e27ccfe7dbb0
harada <mea1238@icloud.com>
2019-05-31 12:09:25 +0900

    AI強さ調整

--- StickHumanFighters/source/AttackState.h
+++ StickHumanFighters/source/AttackState.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "AIState.h"
 
+#include <string>
+
 class AttackState :
 	public AIState
 {
@@ -16,5 +18,6 @@ private:
 	void Update(AICharacter* character, const int level);
 
 	unsigned int attackCount;
+	std::string attackAnimOld;		// 1回前の攻撃のアニメーション情報
 };
 
